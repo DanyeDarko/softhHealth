871ecb54068612944dea03ac2972e88e2bd8ad0b
DanyeDarko <danielbrzen@outlook.com>
2021-02-17 20:35:39 -0600

    disable debug for ampy files transmision

--- boot.py
+++ boot.py
@@ -1,6 +1,6 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
-#import esp
-#esp.osdebug(None)
+import esp
+esp.osdebug(None)
 import uos, machine
 #uos.dupterm(None, 1) # disable REPL on UART(0)
 import gc
@@ -8,4 +8,3 @@ import webrepl
 import pythonSCRIPT
 #webrepl.start()
 gc.collect()
-pythonSCRIPT.conectarWIFI('Huawei-Router','2Cxa3a4op8') 
